79df828099f101dc667b1fd65d0fe775ff1beab1
fanzhouyou <fanzhouyou@gmail.com>
2015-03-02 14:11:39 +0800

    first commit

---
+++ gen/com/liwei/uiversion1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.liwei.uiversion1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/liwei/uiversion1/Set.java
@@ -0,0 +1,28 @@
+package com.liwei.uiversion1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * @Description:TODO
+ * @author:liwei
+ * @time:2015-2-5 обнГ9:57:07
+ */
+public class Set extends Activity {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_set);
+
+	}
+
+}
